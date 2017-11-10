40500d4072179611fa4abd625cae0ac8d522b122
217-2 <linelijun@163.com>
2017-11-10 11:02:06 +0800

    optimize histroy_record

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Jsonp} from "@angular/http";
 import {HttpClient} from "@angular/common/http";
-import $ from 'jquery';
 import 'rxjs/Rx';
 import Clipboard from 'clipboard/dist/clipboard.min.js';
 import md5 from 'md5/md5';
@@ -15,7 +14,8 @@ declare var require: any;
 })
 export class TranslateComponent implements OnInit {
   query = '';
-  result: any;
+  result_dst: any;
+  result_src = '';
   zhORen = 'en';
   history = [];
   like = [];
@@ -43,18 +43,24 @@ export class TranslateComponent implements OnInit {
   }
   on_like() {
     const fav = [];
-    fav.unshift(this.query + ':' + this.result);
+    fav.unshift(this.query + ':' + this.result_dst);
     for (let i = 0; i < fav.length; i++) {
       if (this.like.indexOf(fav[i]) < 0) {
         this.like.unshift(fav[i]);
       }
     }
   }
+  on_clear() {
+    this.like = [];
+  }
+  on_clear_his() {
+    this.history = [];
+  }
   on_change() {
-      if (this.query !== '') {
-        console.log('keyup listener!');
-        this.onsubmit();
-      }
+    if (this.query !== '') {
+      console.log('keyup listener!');
+      this.onsubmit();
+    }
   }
   onsubmit() {
     this.query = this.query.replace(/\n/g, '');
@@ -65,54 +71,61 @@ export class TranslateComponent implements OnInit {
     this.http.get('http://api.fanyi.baidu.com/api/trans/vip/translate' + last + '&callback=JSONP_CALLBACK')
       .map(res => res.json()).subscribe(
       (d) => {
-        this.result = d['trans_result'][0].dst;
+        this.result_src = d['trans_result'][0].src;
+        this.result_dst = d['trans_result'][0].dst;
       },
       (err) => { console.log(err); },
       () => { console.log('Jsonp excute has done'); }
     );
-    if ((this.query + ':' + this.result) !== this.history[0] && this.result) {
-      this.history.unshift(this.query + ':' + this.result);
+    if (this.result_dst) {
+      const last_data = [];
+      last_data.unshift(this.result_src + ':' + this.result_dst);
+      for (let i = 0; i < last_data.length; i++) {
+        if (this.history.indexOf(last_data[i]) < 0) {
+          this.history.unshift(last_data[i]);
+        }
+      }
     }
   }
 }
-    // $.ajax({
-    //   url: 'http://api.fanyi.baidu.com/api/trans/vip/translate',
-    //   type: 'post',
-    //   dataType: 'jsonp',
-    //   data: {
-    //     q: this.query,
-    //     appid: appid,
-    //     salt: salt,
-    //     from: 'auto',
-    //     to: 'zh',
-    //     sign: sign
-    //   },
-    //   success: function (data) {
-    //     console.log(data['trans_result'][0].dst);
-    //   }
-    // });
+// $.ajax({
+//   url: 'http://api.fanyi.baidu.com/api/trans/vip/translate',
+//   type: 'post',
+//   dataType: 'jsonp',
+//   data: {
+//     q: this.query,
+//     appid: appid,
+//     salt: salt,
+//     from: 'auto',
+//     to: 'zh',
+//     sign: sign
+//   },
+//   success: function (data) {
+//     console.log(data['trans_result'][0].dst);
+//   }
+// });
 
 
-    // const header = new Headers({
-    //   data: {
-    //     q: this.query,
-    //     appid: '2015063000000001',
-    //     salt: (new Date).getTime(),
-    //     from: 'en',
-    //     to: 'zh',
-    //     sign: sign1
-    //   }});
-    // // header.append('success',  '(data) => {console.log(data);}');
-    //  this.http.get(encodeURIComponent('http://api.fanyi.baidu.com/api/trans/vip/translate'), {params: {
-    //    data: {
-    //      q: this.query,
-    //      appid: '2015063000000001',
-    //      salt: (new Date).getTime(),
-    //      from: 'en',
-    //      to: 'zh',
-    //      sign: sign1
-    //    }}}).subscribe(
-    //   (data) => {console.log(data['trans_result'][0].dst); }
-    // );
-  // }
+// const header = new Headers({
+//   data: {
+//     q: this.query,
+//     appid: '2015063000000001',
+//     salt: (new Date).getTime(),
+//     from: 'en',
+//     to: 'zh',
+//     sign: sign1
+//   }});
+// // header.append('success',  '(data) => {console.log(data);}');
+//  this.http.get(encodeURIComponent('http://api.fanyi.baidu.com/api/trans/vip/translate'), {params: {
+//    data: {
+//      q: this.query,
+//      appid: '2015063000000001',
+//      salt: (new Date).getTime(),
+//      from: 'en',
+//      to: 'zh',
+//      sign: sign1
+//    }}}).subscribe(
+//   (data) => {console.log(data['trans_result'][0].dst); }
+// );
+// }
 
